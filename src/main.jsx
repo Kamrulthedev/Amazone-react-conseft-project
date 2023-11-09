6cf4b3489bc62452366296762d863c33ca6b4070
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-09 08:33:36 +0600

    secount-commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,46 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+// const createRouter = createBrowserRouter([
+//   {
+//      path:'/',
+//      element: <div>This is my wevsite</div>
+//    },
+//    {
+//    path: '/Prodects',
+//    element: <div>This is my prodects</div>
+//    },
+//    {
+//    path: '/about',
+//    element: <div>This is my about page</div>
+//    }
+// ]);
+
+const uotlet = createBrowserRouter([
+  {
+    path: '/',
+    element: <section>
+      <div>This is page is Fiext page</div>
+     <Outlet></Outlet>
+    </section>,
+
+      
+    children: [{
+         
+      path: '/prodects',
+      element: <div>This is my  prodects</div>
+    }
+  ]
+
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <RouterProvider>
+   <RouterProvider router={uotlet}>
 
    </RouterProvider>
 
